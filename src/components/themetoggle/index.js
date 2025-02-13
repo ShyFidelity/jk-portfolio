925a50a33dc4fef357cd3b4b13eec2588b950cf1
jkworks <info@jilliankayworks.com>
2025-02-13 16:48:31 -0700

    light dark icons

--- src/components/themetoggle/index.js
+++ src/components/themetoggle/index.js
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { WiMoonAltWaningCrescent4 } from "react-icons/wi";
-
+import { WiMoonAltWaningCrescent4, WiDaySunny } from "react-icons/wi";
 
 const Themetoggle = () => {
-  const [theme, settheme] = useState(localStorage.getItem("theme"));
-  const themetoggle = () => {
-    settheme(theme === "dark" ? "light" : "dark");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
   };
+
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme ); 
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
+
   return (
-    <div className="nav_ac" onClick={themetoggle}>
-      <WiMoonAltWaningCrescent4 />
+    <div className="nav_ac" onClick={toggleTheme} style={{ cursor: "pointer", display: "flex", alignItems: "center", gap: "5px" }}>
+      {theme === "light" ? <WiDaySunny /> : null}
+      {theme === "dark" ? <WiMoonAltWaningCrescent4 /> : null}
     </div>
   );
 };
